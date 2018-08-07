6194da375c09d5c4e08e9f2c72c4b6f8c9f079d1
Andrea Baccega <vekexasia@gmail.com>
2018-08-07 12:33:17 +0200

    pm2 kill and sudo request for install

--- scripts/upgrade_to_1.1.1.sh
+++ scripts/upgrade_to_1.1.1.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 # Make sure that if anything fails the command script stops executing
 set -e
+echo ""
 
+if ! sudo -n true 2>/dev/null; then
+    echo "Please provide sudo password for validation"
+    if sudo -Sv -p ''; then
+        echo -e "Sudo authenticated.\t\t\t\t\t$(tput setaf 2)Passed$(tput sgr0)"
+    else
+        echo -e "Unable to authenticate Sudo.\t\t\t\t\t$(tput setaf 1)Failed$(tput sgr0)"
+        exit 2
+    fi
+fi
+
+echo ""
+echo "Please be patient. Installation may take a few minutes depending on your VPS"
+echo ""
 
 if [ ! -d ./rise ]; then
 	echo "You're not in the right folder"
@@ -21,6 +35,9 @@ BACKUP_NAME=$(readlink data/backups/latest)
 BACKUP_FILE="./data/backups/$BACKUP_NAME"
 
 ./manager.sh stop all
+source scripts/env_vars.sh
+# Prevent
+pm2 kill
 
 # Move backup file for later restore
 mv $BACKUP_FILE ../$BACKUP_NAME
@@ -28,6 +45,9 @@ cd ..
 [ -f ./install.sh ] && rm install.sh
 
 mv rise rise_1.0.x
+
+
+
 
 wget https://raw.githubusercontent.com/RiseVision/rise-build/master/scripts/install.sh
 bash install.sh install -r mainnet -u https://downloads.rise.vision/core/mainnet/rise_1.1.1_mainnet_6f40e287.tar.gz
